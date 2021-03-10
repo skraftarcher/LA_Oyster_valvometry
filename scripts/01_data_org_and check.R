fcf77e0272a0d5082ed4d56d1110dc564ba2eef5
skraftarcher <sarcher@lumcon.edu>
2021-03-10 15:47:43 -0600

    commenting code

--- scripts/01_data_org_and check.R
+++ scripts/01_data_org_and check.R
@@ -3,16 +3,25 @@
 
 #initiated by S.K. Archer March 10, 2021
 
-source("scripts/01a_data_org_and check_functions.R")
+source("scripts/01a_data_org_and check_functions.R")# this loads the functions
+# I've written to automatically bring in the data, assign the date and time the
+# measurement was taken, and concatenate datasets downloaded on the same day
 
-org.data(download_date = "031021")
+org.data(download_date = "031021")# to run this function just put the download date
+# that you want to process within the "" dates are in a mmddyy format so the
+# data downloaded on March 10, 2021 is "031021" 
+# you only need to run the function above once for each download date.
 
+# this will bring in the organized data
+# for a different download date just change the bit at the end (i.e., the 031021)
 oys.031021<-read_rds("wdata/data_check_031021.rds")
+
 # This is where you can add oyster specific calibration information when we have it
 
 #PLACEHOLDER
 
-# Look at time series
+
+# Look at time series to make sure things are looking good.
 
 ggplot(oys.031021)+
   geom_line(aes(x=measure_datetime,y=mm,group=oyster))+
--- scripts/01a_data_org_and check_functions.R
+++ scripts/01a_data_org_and check_functions.R
@@ -1,31 +1,36 @@
-get_obs_datetime<-function(d1){d1<-d1%>%
-  mutate(start_datetime=start_time)
+get_obs_datetime<-function(d1){
+  d1<-d1%>%
+  mutate(start_datetime=start_time)# creates a new variable that will have both the date and the time the instrument started recording
 
-year(d1$start_datetime)<-year(d1$start_date)
-month(d1$start_datetime)<-month(d1$start_date)
-day(d1$start_datetime)<-day(d1$start_date)
+year(d1$start_datetime)<-year(d1$start_date) # assigns the correct year
+month(d1$start_datetime)<-month(d1$start_date)# assigns the correct month
+day(d1$start_datetime)<-day(d1$start_date)#assigns the correct day
 
-d1$measure_datetime<-d1$start_datetime+d1$seconds-1
+d1$measure_datetime<-d1$start_datetime+d1$seconds-1 # adds the seconds into the recording and subtracts 1- assuming the instruments takes a measurement when it starts recording.
 
 d1<-d1 %>%
-  pivot_longer(2:5,names_to="oyster",values_to="mm")
+  pivot_longer(2:5,names_to="oyster",values_to="mm")# makes the dataset longer instead of each oyster being a column
 return(d1)
 }
 
 org.data<-function(download_date=NA){
+  # installs (if necessary) and loads required packages
   if(!require(tidyverse))install.packages("tidyverse");library(tidyverse)
   if(!require(lubridate))install.packages("lubridate");library(lubridate)
   if(!require(readxl))install.packages("readxl")
   
+  # find the files to bring in
   fnames<-data.frame(file.name=list.files("odata",pattern=download_date))%>%
     separate(.,file.name,into=c("obj.name","ext"),sep=-5,remove=FALSE)%>%
     mutate(file.name=paste0("odata/",file.name))
+  #import and reorganize the files
   for(i in 1:nrow(fnames)){
     assign(fnames[i,2],readxl::read_xlsx(fnames[i,1]))
     assign(fnames[i,2],get_obs_datetime(get(fnames[i,2])))
   }
-
+  # create one data file per download date
   if(nrow(fnames)>=2) oys<-bind_rows(get(fnames[1,2]),get(fnames[2,2]))
   if(nrow(fnames)==1)oys<-get(fnames[1,1])
+  #save as an rds file
   write_rds(oys,paste0("wdata/data_check_",download_date,".rds"))
 }
