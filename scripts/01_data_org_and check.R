23cd70b2829b6d1038e43f77fdd696051055a44a
skraftarcher <sarcher@lumcon.edu>
2021-04-07 14:37:47 -0500

    update to data formatting/organizing

--- scripts/01_data_org_and check.R
+++ scripts/01_data_org_and check.R
@@ -1,5 +1,4 @@
 #script to organize valvometry data
-# load packages
 
 #initiated by S.K. Archer March 10, 2021
 
@@ -7,24 +6,45 @@ source("scripts/01a_data_org_and check_functions.R")# this loads the functions
 # I've written to automatically bring in the data, assign the date and time the
 # measurement was taken, and concatenate datasets downloaded on the same day
 
-org.data(download_date = "031021")# to run this function just put the download date
+# this first function takes the data as its output by the strainmeter and turns it into a useful format
+# it should be run on each file you download from the strainmeter
+format.data(path="odata/LSU/F005.csv",
+            ch1="o4_1",ch2="o1_1",ch3="o2_1",ch4="o6_1",
+            outfile="odata/LSU_040721.rds")
+
+format.data(path="odata/LUMCON/F007.csv",
+            ch1="o3_1",ch2="o5_1",ch3="o7_1",ch4="o8_1",
+            outfile="odata/LUMCON_040721.rds")
+
+org.data(download_date = "031021",file.type="xlsx")
+org.data(download_date = "031821",file.type="xlsx")
+org.data(download_date = "040721",file.type="rds")# to run this function just put the download date
 # that you want to process within the "" dates are in a mmddyy format so the
 # data downloaded on March 10, 2021 is "031021" 
 # you only need to run the function above once for each download date.
 
+## now link with environmental data if available
+# you only need to run the function below once for each download date.
+
+env.data(oys.file="data_check_031021.rds",
+         env.file="env_data_3_01_10_2021.xlsx",
+         env.sheet="forR",
+         out.file="oys_env_031021.rds")
+
 # this will bring in the organized data
 # for a different download date just change the bit at the end (i.e., the 031021)
-oys.031021<-read_rds("wdata/data_check_031021.rds")
+oys.031021<-read_rds("wdata/oys_env_031021.rds")
+oys.031821<-read_rds("wdata/data_check_031821.rds")
+
+
 
 # This is where you can add oyster specific calibration information when we have it
 
 #PLACEHOLDER
 
 
-# Look at time series to make sure things are looking good.
+# "quick" data look - summarizing data by hour 
+#- plots mean mm per hour only meaningful in a relative sense 
+# mm value is the absolute value
+quick.view(pattrn="data_check_*")
 
-ggplot(oys.031021)+
-  geom_line(aes(x=measure_datetime,y=mm,group=oyster))+
-  facet_wrap(~oyster,scales="free")
-
-ggsave("figures/data_check_031021.jpg")
--- scripts/01a_data_org_and check_functions.R
+++ scripts/01a_data_org_and check_functions.R
@@ -1,36 +1,70 @@
-get_obs_datetime<-function(d1){
-  d1<-d1%>%
-  mutate(start_datetime=start_time)# creates a new variable that will have both the date and the time the instrument started recording
-
-year(d1$start_datetime)<-year(d1$start_date) # assigns the correct year
-month(d1$start_datetime)<-month(d1$start_date)# assigns the correct month
-day(d1$start_datetime)<-day(d1$start_date)#assigns the correct day
-
-d1$measure_datetime<-d1$start_datetime+d1$seconds-1 # adds the seconds into the recording and subtracts 1- assuming the instruments takes a measurement when it starts recording.
-
-d1<-d1 %>%
-  pivot_longer(2:5,names_to="oyster",values_to="mm")# makes the dataset longer instead of each oyster being a column
-return(d1)
+format.data<-function(path,ch1,ch2,ch3,ch4,outfile){
+  library(tidyverse)
+  library(lubridate)
+  t1<-read.csv(path,skip=16)
+  sdt<-read.csv(path,skip=4,nrows=2,header=F)
+  colnames(t1)<-c("seconds",ch1,ch2,ch3,ch4)
+  t1<-t1%>%
+    mutate(start_datetime=mdy_hms(paste(sdt[1,2],sdt[2,2])),
+           measure_datetime=start_datetime+seconds-1)%>%
+    pivot_longer(2:5,names_to="oyster",values_to="mm")
+  #save as an rds file
+  write_rds(t1,outfile)
 }
 
-org.data<-function(download_date=NA){
+org.data<-function(download_date=NA,file.type=NA){
+  library(readxl)
+  library(tidyverse)
+  t1<-data.frame(file=list.files("odata",pattern=download_date))
+  for(i in 1:nrow(t1)){
+    if(file.type=="xlsx")assign(paste0("t",1+i),read_excel(paste0("odata/",t1$file[i])))
+    if(file.type=="rds")assign(paste0("t",1+i),read_rds(paste0("odata/",t1$file[i])))
+    if(i==1)d1<-t2
+    if(i!=1)d1<-bind_rows(t2,get(paste0("t",1+i)))
+  }
+  write_rds(d1,paste0("wdata/data_check_",download_date,".rds"))
+}
+# link with environmental data
+
+env.data<-function(oys.file,env.file,env.sheet,out.file){
   # installs (if necessary) and loads required packages
   if(!require(tidyverse))install.packages("tidyverse");library(tidyverse)
   if(!require(lubridate))install.packages("lubridate");library(lubridate)
   if(!require(readxl))install.packages("readxl")
-  
-  # find the files to bring in
-  fnames<-data.frame(file.name=list.files("odata",pattern=download_date))%>%
-    separate(.,file.name,into=c("obj.name","ext"),sep=-5,remove=FALSE)%>%
-    mutate(file.name=paste0("odata/",file.name))
-  #import and reorganize the files
-  for(i in 1:nrow(fnames)){
-    assign(fnames[i,2],readxl::read_xlsx(fnames[i,1]))
-    assign(fnames[i,2],get_obs_datetime(get(fnames[i,2])))
-  }
-  # create one data file per download date
-  if(nrow(fnames)>=2) oys<-bind_rows(get(fnames[1,2]),get(fnames[2,2]))
-  if(nrow(fnames)==1)oys<-get(fnames[1,1])
-  #save as an rds file
-  write_rds(oys,paste0("wdata/data_check_",download_date,".rds"))
+  od<-read_rds(paste0("wdata/",oys.file))
+  ed<-readxl::read_xlsx(paste0("odata/",env.file),sheet = env.sheet)%>%
+    mutate(env.int=as.numeric(rownames(.)))
+  od<-od%>%
+    mutate(env.int=findInterval(od$measure_datetime,ed$TIMESTAMP))%>%
+    left_join(ed)
+  write_rds(od,paste0("wdata/",out.file))
 }
+
+# concatenate data sets
+bind.data<-function(pattrn){
+  t1<-list.files("wdata",pattern=pattrn)
+  t1<-paste0("wdata/",t1)
+  t2<-read_rds(t1[1])
+  for(i in 2:length(t1))t2<-bind_rows(t2,read_rds(t1[i]))
+  return(t2)
+}
+
+# plot data
+quick.view<-function(pattrn){
+  library(lubridate)
+  library(tidyverse)
+  t1<-bind.data(pattrn=pattrn)
+  t2<-t1%>%
+    mutate(yr=year(measure_datetime),
+           mnth=month(measure_datetime),
+           d=day(measure_datetime),
+           hr=hour(measure_datetime),
+           datetime=ymd_h(paste(yr,mnth,d,hr)))%>%
+    group_by(oyster,datetime)%>%
+    summarize(m.mm=mean(abs(mm)))
+  p1<-ggplot(data=t2)+
+    geom_line(aes(x=datetime,y=m.mm,group=oyster,color=oyster))+
+    facet_wrap(~oyster)
+  return(p1)
+}
+
