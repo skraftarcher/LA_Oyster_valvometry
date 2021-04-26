fd6e519885f614c85430b3fe8a8effaed3d90146
skraftarcher <sarcher@lumcon.edu>
2021-04-26 09:03:17 -0500

    cleanup and streamline data organization

--- scripts/01_data_org_and check.R
+++ scripts/01_data_org_and check.R
@@ -8,48 +8,112 @@ source("scripts/01a_data_org_and check_functions.R")# this loads the functions
 
 # this first function takes the data as its output by the strainmeter and turns it into a useful format
 # it should be run on each file you download from the strainmeter
+# format.data(path="odata/LSU/F001.csv",
+#             ch1="o4_1",ch2="o1_1",ch3="o2_1",ch4="o6_1",
+#             outfile="odata/LSU_031021.rds")
+# 
+# format.data(path="odata/LSU/F004.csv",
+#             ch1="o4_1",ch2="o1_1",ch3="o2_1",ch4="o6_1",
+#             outfile="odata/LSU_031821.rds")
+
 # format.data(path="odata/LSU/F005.csv",
 #             ch1="o4_1",ch2="o1_1",ch3="o2_1",ch4="o6_1",
 #             outfile="odata/LSU_040721.rds")
 
-format.data(path="odata/LSU/F006.csv",
-            ch1="o4_1",ch2="o1_1",ch3="o2_1",ch4="o6_1",
-            outfile="odata/LSU_041421.rds")
+# format.data(path="odata/LSU/F006.csv",
+#             ch1="o4_1",ch2="o1_1",ch3="o2_1",ch4="o6_1",
+#             outfile="odata/LSU_041421.rds")
+
+# format.data(path="odata/LSU/F007.csv",
+#             ch1="o4_1",ch2="o1_1",ch3="o2_1",ch4="o6_1",
+#             outfile="odata/LSU_a_042121.rds")
+# 
+# format.data(path="odata/LSU/F008.csv",
+#             ch1="o4_1",ch2="o1_1",ch3="o2_1",ch4="o6_1",
+#             outfile="odata/LSU_b_042121.rds")
+# 
+# format.data(path="odata/LSU/F009.csv",
+#             ch1="o4_1",ch2="o1_1",ch3="o2_1",ch4="o6_1",
+#             outfile="odata/LSU_c_042121.rds")
+
+
+# format.data(path="odata/LUMCON/F005.csv",
+#             ch1="o3_1",ch2="o5_1",ch3="o7_1",ch4="o8_1",
+#             outfile="odata/LUMCON_031021.rds")
+# 
+# format.data(path="odata/LUMCON/F005.csv",
+#             ch1="o3_1",ch2="o5_1",ch3="o7_1",ch4="o8_1",
+#             outfile="odata/LUMCON_031821.rds")
 
 # format.data(path="odata/LUMCON/F007.csv",
 #             ch1="o3_1",ch2="o5_1",ch3="o7_1",ch4="o8_1",
 #             outfile="odata/LUMCON_040721.rds")
 
-format.data(path="odata/LUMCON/F008.csv",
-            ch1="o3_1",ch2="o5_1",ch3="o7_1",ch4="o8_1",
-            outfile="odata/LUMCON_041421.rds")
-
-# to run this function just put the download date
+# format.data(path="odata/LUMCON/F008.csv",
+#             ch1="o3_1",ch2="o5_1",ch3="o7_1",ch4="o8_1",
+#             outfile="odata/LUMCON_041421.rds")
+# 
+# format.data(path="odata/LUMCON/F009.csv",
+#             ch1="o3_1",ch2="o5_1",ch3="o7_1",ch4="o8_1",
+#             outfile="odata/LUMCON_a_042121.rds")
+# 
+# format.data(path="odata/LUMCON/F010.csv",
+#             ch1="o3_1",ch2="o5_1",ch3="o7_1",ch4="o8_1",
+#             outfile="odata/LUMCON_b_042121.rds")
+# 
+# format.data(path="odata/LUMCON/F011.csv",
+#             ch1="o3_1",ch2="o5_1",ch3="o7_1",ch4="o8_1",
+#             outfile="odata/LUMCON_c_042121.rds")
+# 
+# # to run this function just put the download date
 # that you want to process within the "" dates are in a mmddyy format so the
 # data downloaded on March 10, 2021 is "031021" 
 # you only need to run the function above once for each download date.
 
 
-# org.data(download_date = "031021",file.type="xlsx")
-# org.data(download_date = "031821",file.type="xlsx")
-# org.data(download_date = "040721",file.type="rds")
-# org.data(download_date = "041421",file.type="rds")
+# org.data(download_date = "031021",file.type="rds",sdate = "2021-03-03")
+# org.data(download_date = "031821",file.type="rds",sdate = "2021-03-11")
+# org.data(download_date = "040721",file.type="rds",sdate = "2021-03-19")
+# org.data(download_date = "041421",file.type="rds",sdate = "2021-04-08")
+# org.data(download_date = "042121",file.type="rds",sdate = "2021-04-15")
 
+#if boat detections processed run this code
+
+boat.detections(dataset = "boatnoise_pumphouse.csv",
+                env.file="MC_3-3-21_4-23-21.xlsx",
+                env.sheet="forR",
+                outfile="boat_detections_030221_042121.rds")
 
 ## now link with environmental data if available
 # you only need to run the function below once for each download date.
 
+
 env.data(oys.file="data_check_031021.rds",
-         env.file="env_data_3_01_10_2021.xlsx",
+         env.file="MC_3-3-21_4-23-21.xlsx",
          env.sheet="forR",
          out.file="oys_env_031021.rds")
 
-# this will bring in the organized data
-# for a different download date just change the bit at the end (i.e., the 031021)
-oys.031021<-read_rds("wdata/oys_env_031021.rds")
-oys.031821<-read_rds("wdata/data_check_031821.rds")
+env.data(oys.file="data_check_031821.rds",
+         env.file="MC_3-3-21_4-23-21.xlsx",
+         env.sheet="forR",
+         out.file="oys_env_031821.rds")
 
+env.data(oys.file="data_check_040721.rds",
+         env.file="MC_3-3-21_4-23-21.xlsx",
+         env.sheet="forR",
+         out.file="oys_env_040721.rds")
 
+env.data(oys.file="data_check_041421.rds",
+         env.file="MC_3-3-21_4-23-21.xlsx",
+         env.sheet="forR",
+         out.file="oys_env_041421.rds")
+
+env.data(oys.file="data_check_042121.rds",
+         env.file="MC_3-3-21_4-23-21.xlsx",
+         env.sheet="forR",
+         out.file="oys_env_042121.rds")
+
+ 
 
 # This is where you can add oyster specific calibration information when we have it
 
@@ -61,3 +125,8 @@ oys.031821<-read_rds("wdata/data_check_031821.rds")
 # mm value is the absolute value
 quick.view(pattrn="data_check_*")
 
+
+# this will combine all data into a single dataset
+
+
+combine.dsets(outfile = "all_data_042121.rds")
--- scripts/01a_data_org_and check_functions.R
+++ scripts/01a_data_org_and check_functions.R
@@ -12,16 +12,17 @@ format.data<-function(path,ch1,ch2,ch3,ch4,outfile){
   write_rds(t1,outfile)
 }
 
-org.data<-function(download_date=NA,file.type=NA){
-  library(readxl)
+org.data<-function(download_date=NA,file.type=NA,sdate=NA){
+  if(file.type=="xlsx")library(readxl)
   library(tidyverse)
   t1<-data.frame(file=list.files("odata",pattern=download_date))
   for(i in 1:nrow(t1)){
     if(file.type=="xlsx")assign(paste0("t",1+i),read_excel(paste0("odata/",t1$file[i])))
     if(file.type=="rds")assign(paste0("t",1+i),read_rds(paste0("odata/",t1$file[i])))
     if(i==1)d1<-t2
-    if(i!=1)d1<-bind_rows(t2,get(paste0("t",1+i)))
+    if(i!=1)d1<-bind_rows(d1,get(paste0("t",1+i)))
   }
+  d1<-filter(d1,measure_datetime >= sdate)
   write_rds(d1,paste0("wdata/data_check_",download_date,".rds"))
 }
 # link with environmental data
@@ -33,9 +34,10 @@ env.data<-function(oys.file,env.file,env.sheet,out.file){
   if(!require(readxl))install.packages("readxl")
   od<-read_rds(paste0("wdata/",oys.file))
   ed<-readxl::read_xlsx(paste0("odata/",env.file),sheet = env.sheet)%>%
-    mutate(env.int=as.numeric(rownames(.)))
+    mutate(env.int=as.numeric(rownames(.)),
+           etime = with_tz(TIMESTAMP,tz = "America/Chicago"))
   od<-od%>%
-    mutate(env.int=findInterval(od$measure_datetime,ed$TIMESTAMP))%>%
+    mutate(env.int=findInterval(od$measure_datetime,ed$etime))%>%
     left_join(ed)
   write_rds(od,paste0("wdata/",out.file))
 }
@@ -45,7 +47,7 @@ bind.data<-function(pattrn){
   t1<-list.files("wdata",pattern=pattrn)
   t1<-paste0("wdata/",t1)
   t2<-read_rds(t1[1])
-  for(i in 2:length(t1))t2<-bind_rows(t2,read_rds(t1[i]))
+  if(length(t1)>1)for(i in 2:length(t1))t2<-bind_rows(t2,read_rds(t1[i]))
   return(t2)
 }
 
@@ -68,3 +70,57 @@ quick.view<-function(pattrn){
   return(p1)
 }
 
+#organize boat noise detectections and add environmental interval for linking 
+# with larger dataset
+
+
+boat.detections<-function(dataset,env.file,env.sheet,outfile){
+  if(!require(tidyverse))install.packages("tidyverse");library(tidyverse)
+  if(!require(lubridate))install.packages("lubridate");library(lubridate)
+  ed<-readxl::read_xlsx(paste0("odata/",env.file),sheet = env.sheet)%>%
+    mutate(env.int=as.numeric(rownames(.)),
+           etime = with_tz(TIMESTAMP,tz = "America/Chicago"))
+  bd<-read.csv(paste0("odata/",dataset))%>%
+    separate(file,into = c("soundtrap","yy","mm","dd","hh","min","ss","dash","mplus"),
+             sep=c(5,7,9,11,13,15,17,18),remove=FALSE,convert=TRUE)%>%
+    separate(mplus, into = c("mplus","extns"),sep = -4,convert=TRUE)%>%
+    mutate(boat.datetime = ymd_hm(paste(yy,mm,dd,hh,min)),
+           boat.datetime = boat.datetime + (mplus*60),
+           boat.detected = ifelse(boats == 0,0,1))%>%
+    dplyr::select(soundtrap,boat.datetime,boats,boat.detected)%>%
+    arrange(boat.datetime)
+  bd$env.int = findInterval(bd$boat.datetime,ed$etime)
+  write_rds(bd,paste0("wdata/",outfile))
+}
+
+
+# function to combine all data into a single dataset
+combine.dsets = function(outfile){
+  oe<-bind.data(pattrn = "oys_env_*")
+  bd<-bind.data(pattrn = "boat_detections_*")
+  oe2<-oe %>%
+    filter(!is.na(Water_Temperature_C))%>%
+    filter(env.int != 0)%>%
+    group_by(oyster)%>%
+    mutate(max.mm = max(abs(mm)),
+           min.mm = min(abs(mm)))%>%
+    group_by(oyster,etime,env.int,max.mm)%>%
+    summarize(wind.spd = mean(Wind_Speed),
+           water.dpth = mean(Water_Depth_m),
+           water.temp = mean(Water_Temperature_C),
+           salinity = mean(Salinity),
+           dox = mean(Dissolved_Oxygen),
+           chla = mean(Chlorophyll),
+           mm.mean = mean(abs(mm)),
+           mm.median = median(abs(mm)),
+           mm.sd = sd(abs(mm)),
+           p.max = mean(abs(mm)/max.mm),
+           p.min = mean((min.mm+.1)/(abs(mm)+.1)))
+  bd2<-bd%>%
+    filter(env.int != 0)%>%
+    group_by(env.int)%>%
+    summarize(m.boat = sum(boat.detected))%>%
+    mutate(m.boat = ifelse(m.boat > 15,15,m.boat))
+  oe3<-left_join(oe2,bd2)
+  write_rds(oe3,paste0("wdata/",outfile))
+}
